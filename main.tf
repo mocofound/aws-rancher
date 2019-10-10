ee6b36390924bfae3b5d1cecd5e00198349ab3d6
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-10-09 19:31:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "rancher2" {
-  api_url    = "https://rancher.my-domain.com"
-  access_key = "${var.rancher2_access_key}"
-  secret_key = "${var.rancher2_secret_key}"
+  #api_url    = "https://rancher.my-domain.com"
+  #access_key = "${var.rancher2_access_key}"
+  #secret_key = "${var.rancher2_secret_key}"
+}
+
+# Create a new rancher2 imported Cluster
+resource "rancher2_cluster" "foo-imported" {
+  name = "foo-imported"
+  description = "Foo rancher2 imported cluster"
 }
